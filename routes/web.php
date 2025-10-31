3026c9df2c536e149110a1f9c7fa9218fa366777
luuamardya18 <luluamardiya4@gmail.com>
2025-10-31 07:21:45 +0700

    Add Feature Read Student

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\StudentController;
 
 Route::get('/', [LandingController::class, 'index'])->name('landing');
 
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-});
+Route::prefix('admin')
+    ->name('admin.') // 👉 tambahkan prefix nama di sini
+    ->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+        // Untuk CRUD table Student
+        Route::resource('students', StudentController::class);
+    });
